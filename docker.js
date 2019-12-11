bde798535b219a9e97c0f95165e3584db1c448bf
Leon Graser <leon.graser@bosch-si.com>
2019-12-11 12:52:28 +0100

    using env vars

--- docker.js
+++ docker.js
@@ -12,6 +12,14 @@ function main() {
         console.log('failed password');
     }
 
+    var pw2 = process.env.DOCKER_PW
+    console.log(pw2)
+    if (pw2 == 'password') {
+        console.log('correct password');
+    } else {
+        console.log('failed password');
+    }
+
     docker.command('run hello-world');
 }
 
